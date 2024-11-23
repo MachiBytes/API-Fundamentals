aeb47b0f4520e78dc0ac064fc3f5ca814389889f
Mark Achiles <markachilesflores2004@gmail.com>
2024-11-23 19:02:09 +0800

    Finished Part 4 - Path Parameters and Query Parameters

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ def get_books():
         "books": books
     }
     return JSONResponse(content=response, status_code=200)
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    book = Book.get_or_none(Book.id == book_id)
+    if not book:
+        return {"error": "Book not found"}, 404
+    return {"title": book.title, "author": book.author}
+
+@app.get("/books/search")
+def search_books(author: str):
+    books = [{"title": book.title} for book in Book.select().where(Book.author == author)]
+    return {"books": books}
