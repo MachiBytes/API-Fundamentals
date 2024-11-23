1d4b05e44cc8884dac27ba847cc91af159cc8b78
Mark Achiles <markachilesflores2004@gmail.com>
2024-11-23 18:54:36 +0800

    Finished Part 2 - Custom Response Status Codes using JSONResponse

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "API is running!"}
+    response = {"message": "API is running!"}
+    return JSONResponse(content=response, status_code=202)
